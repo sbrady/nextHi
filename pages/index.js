cc7cc669826929316d73ea3e608f23d8872c84d1
sbrady <sean.brady@acloud.guru>
2019-05-02 16:34:35 +1000

    adds an ajax request

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,44 @@
 import Link from 'next/link'
 
-function Home() {
-  return (
 
-    <>
-      <div>Welcome to Next.js!</div>
+class Home extends React.Component {
 
-      <Link href='/about'>About Page</Link>
-    </>
+  state = {}
 
-  )
+  render() {
+    return (
+      <>
+        <div>Welcome to Next.js!</div>
+        <Link href='/about'>About Page</Link>
+        <br/>
+        <h1>HumansBeingBros</h1>
 
+
+        {this.state.bros? <div>{this.state.bros }</div>: <div>Loading...</div>}
+
+
+      </>
+
+    );
+  }
+
+  componentDidMount() {
+
+
+    fetch('https://www.reddit.com/r/HumansBeingBros.json')
+      .then(function(response) {
+        return response.json();
+      })
+      .then((myJson)=> {
+
+
+        let titles = myJson.data.children.map(child => child.data).map(data=>data.title)
+
+        this.setState({bros: titles})
+        console.log(JSON.stringify(titles));
+      });
+
+  }
 }
 
 export default Home
